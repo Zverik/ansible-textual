239f1dbd7994639063a8e6e61bcb47f3470f2e5e
Ilya Zverev <zverik@textual.ru>
2024-06-25 23:08:08 +0300

    OSM Auth, Radio, WhosThat

---
+++ roles/radio/files/theme/theme-info.php
@@ -0,0 +1,19 @@
+<?php return array (
+
+  'display_name' => array (
+    'en' => 'Radio OSM Rus',
+    'ru' => 'Радио OSM Rus',
+  ),
+
+  'colors' => array (
+    'background' => '#fff',
+    'headings' => '#000',
+    'text' => '#000',
+    'link' => '#0060a0',
+  ),
+
+  'based_on' => 'plain',
+  'meta_viewport' => 'width=device-width, initial-scale=1',
+  'supports_dark_mode' => true,
+
+); ?>
