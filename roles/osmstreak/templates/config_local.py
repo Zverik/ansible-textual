a5ac9c9d98052799820ec65933bea88af80679b2
Ilya Zverev <zverik@textual.ru>
2024-06-27 17:52:41 +0300

    Started working on osm streak, and fixed backups in iodb

---
+++ roles/osmstreak/templates/config_local.py
@@ -0,0 +1,6 @@
+DEBUG = False
+OAUTH_KEY = "{{ osmstreak_client_id }}"
+OAUTH_SECRET = "{{ osmstreak_client_secret }}"
+SECRET_KEY = "{{ osmstreak_secret }}"
+TELEGRAM_TOKEN = "{{ osmstreak_telegram_token }}"
+BASE_URL = "https://{{ domain }}/"
