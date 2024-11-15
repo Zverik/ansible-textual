ded81e210f2d59b11336db81b0efc626fd75b634
Ilya Zverev <zverik@textual.ru>
2024-11-15 12:22:59 +0200

    Some schedule updates and osmstreak fixes

--- roles/osmstreak/templates/config_local.py
+++ roles/osmstreak/templates/config_local.py
@@ -1,4 +1,5 @@
 DEBUG = False
+DATABASE_URI = "mysql://{{ osmstreak_mysql_user }}:{{ osmstreak_mysql_password }}@localhost/{{ osmstreak_mysql_db }}"
 OAUTH_KEY = "{{ osmstreak_client_id }}"
 OAUTH_SECRET = "{{ osmstreak_client_secret }}"
 SECRET_KEY = "{{ osmstreak_secret }}"
--- roles/schedules/files/update_schedules.sh
+++ roles/schedules/files/update_schedules.sh
@@ -12,11 +12,14 @@ mkdir -p "$DATA"
 # wget -q -r -O "$DATA/f2022-at.xml" "https://talks.osgeo.org/foss4g-2022-academic-track/schedule/export/schedule.xml"
 # wget -q -r -O "$DATA/sotm2020-add.csv" 'https://docs.google.com/spreadsheets/d/1EO3aC3vF9dvb1wopT_cUHaDXqdGIrid_Kj59PZyzR_g/export?format=csv&id=0'
 # sed -i -e 's/Academic Track | Track 2 - Sunday, July 5/Track 2/' "$DATA/sotm2020-at.xml"
-# wget -q -r -O "$DATA/tartu2024.json" "https://talks.osgeo.org/foss4g-europe-2024/schedule/export/schedule.json"
-# wget -q -r -O "$DATA/tartu2024w.json" "https://talks.osgeo.org/foss4g-europe-2024-workshops/schedule/export/schedule.json"
-# wget -q -r -O "$DATA/tartu2024a.csv" 'https://docs.google.com/spreadsheets/d/1Jp88ifOSrYb2T93YTQt9YV0bk7MEZWO6eMtOvKKwdQ4/gviz/tq?tqx=out:csv'
-wget -q -r -O "$DATA/sotmeu24.json" 'https://cfp.openstreetmap.org.pl/state-of-the-map-europe-2024/schedule/export/schedule.json'
-wget -q -r -O "$DATA/sotmeu24.csv" 'https://docs.google.com/spreadsheets/d/1Uwqw5-i78z315syTjlrMP7n2UKrUV8i_zuLeqCa0_Mk/gviz/tq?tqx=out:csv'
+wget -q -r -O "$DATA/f2024.json" "https://talks.osgeo.org/foss4g-2024/schedule/export/schedule.json"
+wget -q -r -O "$DATA/f2024w.json" "https://talks.osgeo.org/foss4g-2024-workshop/schedule/export/schedule.json"
+wget -q -r -O "$DATA/f2024a.json" "https://talks.osgeo.org/foss4g-2024-academic-track/schedule/export/schedule.json"
+#wget -q -r -O "$DATA/sotmeu24.json" 'https://cfp.openstreetmap.org.pl/state-of-the-map-europe-2024/schedule/export/schedule.json'
+#wget -q -r -O "$DATA/sotmeu24.csv" 'https://docs.google.com/spreadsheets/d/1Uwqw5-i78z315syTjlrMP7n2UKrUV8i_zuLeqCa0_Mk/gviz/tq?tqx=out:csv'
+#wget -q -r -O "$DATA/sotm2024.xml" "https://pretalx.com/sotm2024/schedule/export/schedule.xml"
+#wget -q -r -O "$DATA/sotm2024a.xml" "https://pretalx.com/state-of-the-map-2024-academic-track/schedule/export/schedule.xml"
+#wget -q -r -O "$DATA/sotm2024a.csv" 'https://docs.google.com/spreadsheets/d/1vUTN-TG6Mg4gX0tOpQcT4ZrMK-v3X7fHZXWap3HFpnk/gviz/tq?tqx=out:csv'
 
 SC="$VENV/bin/schedule_convert"
-$SC $DATA/sotmeu24.ini $DATA/sotmeu24.json $DATA/sotmeu24.csv -l "$WWW" "$BASEURL/sotmeu24"
+$SC $DATA/f2024.ini $DATA/f2024.json $DATA/f2024a.json $DATA/f2024w.json -l "$WWW" "$BASEURL/f2024"
