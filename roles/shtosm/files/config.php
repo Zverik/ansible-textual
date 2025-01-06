a960d7390e87b306f97c27ab4d4d5b3b2c7fd4a7
Ilya Zverev <ilya@zverev.info>
2025-01-06 12:51:03 +0200

    Finally published shtosm

--- roles/shtosm/files/config.php
+++ roles/shtosm/files/config.php
@@ -5,5 +5,8 @@
 // а потом вписать их сюда с новыми значениями
 $_config['db_table_prefix'] = 'e2';
 $_config['index_follow_everything'] = true;
+$_config['url_composition'] = 'synthetic'; // doesn't detect mod_rewrite
+$_config['index_follow_everything'] = true;
+
 
 ?>
